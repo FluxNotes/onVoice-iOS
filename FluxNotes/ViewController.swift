973961365eb4ca1c4ffe0b1641e7f09973929f29
Robert Horn <rhorn@mitre.org>
2018-08-17 15:46:52 -0400

    Initial Commit

---
+++ FluxNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FluxNotes
+//
+//  Created by Horn III, Robert E on 8/17/18.
+//  Copyright © 2018 Robert Horn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
