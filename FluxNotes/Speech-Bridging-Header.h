1a8540744d3b77c89f30968bcc7760b37cc009c3
Robert Horn <rhorn@mitre.org>
2018-08-17 18:54:14 -0400

    add googleapis dependencies

---
+++ FluxNotes/Speech-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Speech-Bridging-Header.h
+//  FluxNotes
+//
+//  Created by Horn III, Robert E on 8/17/18.
+//  Copyright © 2018 Robert Horn. All rights reserved.
+//
+
+#ifndef Speech_Bridging_Header_h
+#define Speech_Bridging_Header_h
+
+#import <googleapis/CloudSpeech.pbobjc.h>
+#import <googleapis/CloudSpeech.pbrpc.h>
+
+#endif /* Speech_Bridging_Header_h */
